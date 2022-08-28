db3f89073d6077a78e7f0cdbe7ce93ee05338ba8
Felipe Salinas <70719926+piflens@users.noreply.github.com>
2022-08-28 15:17:26 -0400

    Agregado un script js para boton, agregado css para estilos en una pagina, jumbotron, carrusel formularios

---
+++ js/cambiotextos.js
@@ -0,0 +1,10 @@
+//AREA HOTELES
+function VerOpciones() {
+    var cambio = document.getElementById('Ver');
+    if (cambio.innerText == 'Ver Menos')
+        cambio.innerText = 'Ver más opciones';
+    else cambio.innerText = 'Ver Menos';
+ }
+
+ 
+   
